bfbd33789881532880539b575732ba621f542f78
Nikita Yatskovets <NIKYACK@live.spbstu.ru>
2016-12-30 01:36:11 +0300

    Update DbJobs.java

--- DbJobs.java
+++ DbJobs.java
@@ -5,16 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Дима
- */
 public class DbJobs {
     Connection connection;
     private final String host = "jdbc:mysql://localhost:3306/test";
