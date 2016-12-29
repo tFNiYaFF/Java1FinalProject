2afec65ebe81d88a3adc1694427a4af7590d121f
Nikita Yatskovets <NIKYACK@live.spbstu.ru>
2016-12-30 01:37:33 +0300

    Update OneConnection.java

--- OneConnection.java
+++ OneConnection.java
@@ -9,16 +9,7 @@ import java.sql.Connection;
 import java.util.HashMap;
 import java.util.Map;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Дима
- */
 public class OneConnection extends Thread {
     private static DbJobs db;
     private static Map<Integer, DataOutputStream> online = new HashMap<>(); //тут список пользователей онлайн
